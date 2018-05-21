a7ad1f7983320204218f97b6f90ed6145431ba4c
Jalani Paul <jalani2727@gmail.com>
2018-05-21 11:01:43 -0400

    removes big image from the center when clicked on

--- fakeInstagram.js
+++ fakeInstagram.js
@@ -1,8 +1,22 @@
 var imageSelector = "[data-pictureBox]";
+var bigImageSelector = "[data-target]";
+var moveToFront= "[data-front]";
+
 var imageArray = document.querySelectorAll(imageSelector);
+var bigImageTarget = document.querySelector(bigImageSelector);
+var bigImageFrame = document.querySelector(moveToFront);
+
 imageArray.forEach(function (lol) {
     lol.addEventListener('click', function(clickEvent){
         clickEvent.preventDefault();
-        lol.classList.add('imageZoom');
+        bigImageTarget.classList.add('bigContent');
+        bigImageTarget.setAttribute('src', lol.getAttribute('src')); 
+        bigImageFrame.classList.add('moveToFront');
     });
+});
+
+// Makes it so that when i click on the image that has been moved to the front, it will remove the class that has brought it to the front.
+bigImageTarget.addEventListener('click', function(clickEvent){
+    clickEvent.preventDefault();
+    bigImageFrame.classList.remove('moveToFront');
 });
