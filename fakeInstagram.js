0b34309b16aec17129557258456e89469d1f9e0d
Jalani Paul <jalani2727@gmail.com>
2018-06-20 10:44:43 -0400

    idk what's in this ocmmit tbh

--- fakeInstagram.js
+++ fakeInstagram.js
@@ -1,3 +1,6 @@
+// JavaScript code without JQuery Functions
+// JQuery section added below
+
 var imageSelector = "[data-pictureBox]";
 var bigImageSelector = "[data-target]";
 var moveToFront= "[data-front]";
@@ -17,7 +20,7 @@ imageArray.forEach(function (lol) {
     });
 });
 
-// Makes it so that when i click on the image that has been moved to the front, it will remove the class that has brought it to the front.
+// Makes it so that when i click on the image that has been moved to the front, it will remove the class that brought it to the front.
 
 bigImageTarget.addEventListener('click', function(clickEvent){
     clickEvent.preventDefault();
@@ -27,4 +30,15 @@ bigImageTarget.addEventListener('click', function(clickEvent){
 xFrame.addEventListener('click', function(clickEvent){
     clickEvent.preventDefault();
     bigImageFrame.classList.remove('moveToFront');
+});
+
+
+// JQuery
+
+// why doesn't this work?
+// is active but only does a thing on the first img element in the pictureBox
+// currently makes the first image spaz tf out
+$('#smallPics').hover(function(lol) {
+    var toggleIt = $('#smallPics').css('transform', 'scale(1.1)');
+    toggleIt.toggle();
 });
